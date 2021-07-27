44946b5ef529fdf037e93f393db7e847e30d9fa6
Tedd Ho-Jeong An <tedd.an@intel.com>
2021-07-27 16:51:20 -0700

    Bluetooth: btintel: Add btintel data struct
    This patch adds a data structure for btintel for btintel object, and the
    definition of bootloder states.
    
    Signed-off-by: Tedd Ho-Jeong An <tedd.an@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -4133,7 +4133,7 @@ static int btusb_probe(struct usb_interface *intf,
 	struct btusb_data *data;
 	struct hci_dev *hdev;
 	unsigned ifnum_base;
-	int i, err;
+	int i, err, priv_size;
 
 	BT_DBG("intf %p id %p", intf, id);
 
@@ -4219,6 +4219,8 @@ static int btusb_probe(struct usb_interface *intf,
 	init_usb_anchor(&data->ctrl_anchor);
 	spin_lock_init(&data->rxlock);
 
+	priv_size = 0;
+
 	if (id->driver_info & BTUSB_INTEL_NEW) {
 		data->recv_event = btusb_recv_event_intel;
 		data->recv_bulk = btusb_recv_bulk_intel;
@@ -4228,7 +4230,7 @@ static int btusb_probe(struct usb_interface *intf,
 		data->recv_bulk = btusb_recv_bulk;
 	}
 
-	hdev = hci_alloc_dev(0);
+	hdev = hci_alloc_dev(priv_size);
 	if (!hdev)
 		return -ENOMEM;
 
