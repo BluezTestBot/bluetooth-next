f855e86770e58b9a2527321f1116c1a5ceffeb0b
Brian Gix <brian.gix@intel.com>
2022-07-29 17:50:34 -0700

    Bluetooth: Move Adv Instance timer to hci_sync
    The Advertising Instance expiration timer adv_instance_expire was
    handled with the deprecated hci_request mechanism, rather than it's
    replacement: hci_sync.
    
    Signed-off-by: Brian Gix <brian.gix@intel.com>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2524,8 +2524,6 @@ struct hci_dev *hci_alloc_dev_priv(int sizeof_priv)
 	INIT_DELAYED_WORK(&hdev->cmd_timer, hci_cmd_timeout);
 	INIT_DELAYED_WORK(&hdev->ncmd_timer, hci_ncmd_timeout);
 
-	hci_request_setup(hdev);
-
 	hci_init_sysfs(hdev);
 	discovery_init(hdev);
 
